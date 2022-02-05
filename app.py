ce9e7da4dbe0c9b0dbf24d546fabea8ca62fa8a7
Codrut Iftode <codriniftode@gmail.com>
2022-02-05 17:59:24 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/api/hello')
+def api():
+    return "Hey"
+
+# @app.route('/api')
+# def api():
+#     with open('data.json', mode='r') as my_file:
+#         text = my_file.read()
+#         return text
+
+@app.route('/')
+def home():
+    return 'Home Page Route'
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
