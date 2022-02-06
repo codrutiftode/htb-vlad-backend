55c74b70354f80a6df9128528c6bb4cd08fb047d
Ross Bathgate <bathgate-r@outlook.com>
2022-02-06 03:31:04 +0000

    deleted ross-test.py

--- index.py
+++ index.py
@@ -19,6 +19,30 @@ def api():
 #         text = my_file.read()
 #         return text
 
+def parseRecipeIngredients(recipe):
+    ingredients = recipe['ingredients']
+    for i in range(len(ingredients)):
+        ingredients[i] = ingredients[i].split(" ")[1:]
+        ingredients[i] = ' '.join(ingredients[i])
+    return ingredients
+
+
+def getRecipeRanks(ingredients, recipes):
+    ranks = [0 for i in range(len(recipes))]
+    for r in range(len(ranks)):
+        rank = 0
+        parsedIngredients = parseRecipeIngredients(recipes[r])
+        for ingredient in ingredients:
+            for i in parsedIngredients:
+                if ingredient in i:
+                    rank += 1
+        ranks[r] = rank
+    return ranks
+
+
+    
+
+
 @app.route('/api/getAllIngredients')
 def getAllIngredients():
     global allIngredients
